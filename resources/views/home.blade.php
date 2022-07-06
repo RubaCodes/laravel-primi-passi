fa80f460ee60a6ecf6b97092168b030756dd8a6d
rubacodes <rubacodes@gmail.com>
2022-07-06 13:53:13 +0200

    push finale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Home</title>
 </head>
-<body class="h-[100vh]"> 
+<body class="h-[100vh] bg-[#171923] text-white"> 
     <main class="h-full flex  flex-col items-center justify-center gap-8">
-        <h1 class="text-3xl uppercase">home</h1>
-        <ul class="flex gap-4">
-            <li><a href="/chi-siamo">Chi Siamo</a></li>
-            <li><a href="/prodotti">Prodotti</a></li>
-            <li><a href="/contattaci">Contattaci</a></li>
-        </ul>
+        <div class="flex items-center">
+        <i class="devicon-laravel-plain-wordmark colored text-7xl"></i>
+        <h1 class="text-8xl uppercase">home</h1>
+        </div>
+        <nav>
+            <ul class="flex gap-4">
+                <li><a class="block px-4 py-2 rounded  transition-all bg-red-600  ease-in-out  hover:bg-red-500 hover:scale-110 " href="/chi-siamo">Chi Siamo</a></li>
+                <li><a class="block px-4 py-2 rounded  transition-all bg-red-600  ease-in-out  hover:bg-red-500 hover:scale-110" href="/prodotti">Prodotti</a></li>
+                <li><a class="block px-4 py-2 rounded  transition-all bg-red-600  ease-in-out  hover:bg-red-500 hover:scale-110" href="/contattaci">Contattaci</a></li>
+            </ul>
+        </nav>
     </main> 
-
 </body>
 </html>
