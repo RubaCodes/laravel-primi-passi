de34c08a0729416a39a9e079f64b04df419015c0
rubacodes <rubacodes@gmail.com>
2022-07-06 13:34:29 +0200

    style e content home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,15 @@
       <script src="https://cdn.tailwindcss.com"></script>
     <title>Home</title>
 </head>
-<body>  
-        <h1>home</h1>
+<body class="h-[100vh]"> 
+    <main class="h-full flex  flex-col items-center justify-center gap-8">
+        <h1 class="text-3xl uppercase">home</h1>
+        <ul class="flex gap-4">
+            <li><a href="/chi-siamo">Chi Siamo</a></li>
+            <li><a href="/prodotti">Prodotti</a></li>
+            <li><a href="/contattaci">Contattaci</a></li>
+        </ul>
+    </main> 
+
 </body>
 </html>
