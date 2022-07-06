eb7d01e950206200fcfe801e51ce9f8c45645818
rubacodes <rubacodes@gmail.com>
2022-07-06 13:37:54 +0200

    aggiunta piccola nav

---
+++ resources/views/chi-siamo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chi siamo</title>
+</head>
+<body>
+    <h1>Chi siamo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae officia quae modi nam, facilis in perferendis saepe ducimus cum repellendus ipsum fugit eius esse ipsa nobis blanditiis? Ab, alias natus?</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/chi-siamo', function () {
+    return view('chi-siamo');
+});
+Route::get('/prodotti', function () {
+    return view('prodotti');
+});
+
+Route::get('/contattaci', function () {
+    return view('contattaci');
+});
